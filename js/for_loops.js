13bc07a542894b87285249095707828e4fde4d6e
Billy <billy_white.88@yahoo.com>
2018-01-30 16:19:47 -0600

    "review over for_loop"

--- js/for_loops.js
+++ js/for_loops.js
@@ -26,9 +26,7 @@ for(var i = 1; i <= 10; i++) {
 }
 
 for(var i = 1; i <= 9; i +=1) {
-    // .repeat will repeat a string n number of times..
-    // change the number behind i into a string...
-    // output that string.repeat()
+
     var numberString = i.toString();
 
     var output = numberString.repeat(i);
