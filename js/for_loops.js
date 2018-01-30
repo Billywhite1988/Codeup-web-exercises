1a4c0847bf77b936a085a48f75150ea64ca9ebf4
Billy <billy_white.88@yahoo.com>
2018-01-30 14:42:06 -0600

    completed first problem of for_loop ex

---
+++ js/for_loops.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+
+function showMultiplicationTable(number) {
+
+    for(var i = 1; i <= 10; i+=1) {
+        console.log(number + " x " + i + " = " + (number * i));
+    }
+}
+
+showMultiplicationTable(7);
+
+
+
+
+
+
