79d7fe643e77d3f13d5cedc4dedd7d9a59c39c5a
Billy <billy_white.88@yahoo.com>
2018-01-30 16:06:19 -0600

    "review over for_loop"

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,17 +2,35 @@
 
 
 
-function showMultiplicationTable(number) {
+    function showMultiplicationTable(number) {
 
-    for(var i = 1; i <= 10; i+=1) {
+    for(var i = 1; i <= 10; i++) {
         console.log(number + " x " + i + " = " + (number * i));
+    }
+    }
+
+    showMultiplicationTable(7);
+
+
+
+
+for(var i = 1; i <= 10; i++) {
+
+    var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+    if(randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even");
+    } else {
+        console.log(randomNumber + " is odd.");
     }
 }
 
-showMultiplicationTable(7);
+for(var i = 1; i <= 9; i +=1) {
+    // .repeat will repeat a string n number of times..
+    // change the number behind i into a string...
+    // output that string.repeat()
+    var numberString = i.toString();
 
-
-
-
-
-
+    var output = numberString.repeat(i);
+    console.log(output);
+}
