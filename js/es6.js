219ef352f5b47cdc75d9e825c5e169b9f3ee9b72
Billy <billy_white.88@yahoo.com>
2018-02-20 15:35:26 -0600

    "review format es6"

--- js/es6.js
+++ js/es6.js
@@ -51,18 +51,15 @@ let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(user => {
-    return emails.push(user.email);
-});
-
-users.forEach((user) => {
-    return names.push(user.name);
+    emails.push(user.name);
+    names.push(user.name);
 });
 
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
 
-users.forEach(function(user) {
+
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
@@ -78,7 +75,7 @@ users.forEach(function(user) {
 //   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
 // });
     developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(", ")}.`);
-});
+
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
@@ -94,3 +91,4 @@ let list = '<ul>';
     }
 list += '</ul>';
 
+    document.getElementsByTagName('body')[0].innerHTML = list;
